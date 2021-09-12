cd6a663e1948eae72c50bb330b1c3a1e77617c88
Willie Johnson <liwa.johnson@gmail.com>
2021-09-12 15:31:53 -0400

    Make editor and footer a z stack

--- Espy/Views/EditView.swift
+++ Espy/Views/EditView.swift
@@ -1,9 +1,9 @@
-//
-//  EditView.swift
-//  Espy
-//
-//  Created by Willie Johnson on 9/1/21.
-//
+  //
+  //  EditView.swift
+  //  Espy
+  //
+  //  Created by Willie Johnson on 9/1/21.
+  //
 
 import SwiftUI
 
@@ -60,7 +60,7 @@ struct EditView: View {
 
   var body: some View {
     VStack(alignment: .center, spacing: 4.0, content: {
-      // Header
+        // Header
       HStack {
         TrashButton(onPress: {
           if (!isNew) {
@@ -75,56 +75,56 @@ struct EditView: View {
         EditModeButton()
       }.padding()
 
-      QuickTextEditor(text: $fullText, activeFont: $activeFont, placeholder: "# Untitled") { value in
-        isTextUpdated = fullText != originalText
-        currentDate = Date()
-      }
-      .focused($isEditingText)
-      .cornerRadius(10)
-      .padding(10)
-
-      // Footer
-
-      VStack {
-        VStack(alignment: .center, spacing: 1) {
-          Text(selectedEntry.id.uuidString).padding(0).font(.subheadline).foregroundColor(isNew ? .green : .gray)
-          Text(currentDate.formattedStringDate()).padding(5).font(.caption).foregroundColor((isTextUpdated || isNew) ? .green : .gray)
+      ZStack(alignment: .bottomLeading) {
+        QuickTextEditor(text: $fullText, activeFont: $activeFont, placeholder: "# Untitled") { value in
+          isTextUpdated = fullText != originalText
+          currentDate = Date()
         }
-        HStack {
-          ImportButton(onPress: {
-            print("eh")
-          })
-          Spacer()
-          Button(action: {
-            if isNew {
-              self.selectedEntry.update(with: fullText)
-              mainManager.add(entry: selectedEntry)
-            } else if isTextUpdated {
-              mainManager.update(entry: selectedEntry, with: fullText)
-            }
-            contentManager.unselect(selectedEntry)
-            presentationMode.wrappedValue.dismiss()
-          }, label: {
-            Image(systemName: "chevron.compact.down")
-              .font(.system(size: 44.0, weight: .bold)).foregroundColor((isTextUpdated || isNew) ? .green : .accentColor)
-          })
-          Spacer()
-          ExportButton()
+        .focused($isEditingText)
+        .cornerRadius(10)
+        .padding(10)
+          // Footer
+        VStack {
+          VStack(alignment: .center, spacing: 1) {
+            Text(selectedEntry.id.uuidString).padding(0).font(.subheadline).foregroundColor(isNew ? .green : .gray)
+            Text(currentDate.formattedStringDate()).padding(5).font(.caption).foregroundColor((isTextUpdated || isNew) ? .green : .gray)
+          }
+          HStack {
+            ImportButton(onPress: {
+              print("eh")
+            })
+            Spacer()
+            Button(action: {
+              if isNew {
+                self.selectedEntry.update(with: fullText)
+                mainManager.add(entry: selectedEntry)
+              } else if isTextUpdated {
+                mainManager.update(entry: selectedEntry, with: fullText)
+              }
+              contentManager.unselect(selectedEntry)
+              presentationMode.wrappedValue.dismiss()
+            }, label: {
+              Image(systemName: "chevron.compact.down")
+                .font(.system(size: 44.0, weight: .bold)).foregroundColor((isTextUpdated || isNew) ? .green : .accentColor)
+            })
+            Spacer()
+            ExportButton()
+          }
         }
+        .padding(20)
       }
-      .padding(20)
     })
-    .onAppear(perform: {
-      if (!isNew) {
-        contentManager.select(selectedEntry)
-      } 
-    })
+      .onAppear(perform: {
+        if (!isNew) {
+          contentManager.select(selectedEntry)
+        }
+      })
   }
 }
 
 @available(iOS 15.0, *)
 struct EditView_Previews: PreviewProvider {
   static var previews: some View {
-        EditView()
-    }
+    EditView()
+  }
 }
